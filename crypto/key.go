e2f6ba25bf7f652dec3dcf1f94be08dfad995f7e
ANNMAINAWANGARI <wangariann371@gmail.com>
2024-01-30 13:26:49 +0300

    keys testing and serialization

--- crypto/key.go
+++ crypto/key.go
@@ -27,6 +27,23 @@ type PrivateKey struct {
 	value []byte
  }
 
+ func NewPrivateKeyFromString(s string) *PrivateKey{
+	b, err := hex.DecodeString(s)
+	if err != nil{
+		panic(err)
+	}
+	return NewPrivateKeyFromSeed(b)
+ }
+
+ func NewPrivateKeyFromSeed(seed []byte) *PrivateKey{
+	if len(seed) != seedLen {
+		panic("invalid seed length, must be 32")
+	}
+	return &PrivateKey{
+		key: ed25519.NewKeyFromSeed(seed),
+	}
+ }
+
  func GeneratePrivateKey() *PrivateKey{
 	seed:=make([]byte,seedLen)
 	_,err:=io.ReadFull(rand.Reader,seed)
--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -1,6 +1,7 @@
 package crypto
 
 import (
+	
 	"fmt"
 	"testing"
 
@@ -12,6 +13,19 @@ func TestGeneratePrivateKey(t *testing.T) {
   assert.Equal(t, len(privKey.Bytes()),privKeyLen)
   pubKey :=privKey.Public()
   assert.Equal(t,len(pubKey.Bytes()),pubKeyLen)
+}
+
+func TestNewPrivateKeyFromString(t *testing.T){
+	var(
+		seed = "2584b8e72a2c5b639158b1014f83df1cc4882ce55f280d960bcde05fca5cc261"
+	    privKey = NewPrivateKeyFromString(seed)
+		addressStr = "e098692712786535e453646f8dc4a6e5e54d1e8f"
+	)
+	
+	assert.Equal(t, privKeyLen, len(privKey.Bytes()))
+	address := privKey.Public().Address()
+	assert.Equal(t, addressStr, address.String())
+	
 }
 
 func TestPrivateKeySign(t *testing.T){
